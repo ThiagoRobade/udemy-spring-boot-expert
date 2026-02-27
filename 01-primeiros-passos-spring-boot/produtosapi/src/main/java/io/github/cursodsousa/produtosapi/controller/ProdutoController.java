d701a8a290f02a453e8b7f46edacbd9def7c4166
ThiagoRobade <robade@live.com>
2026-02-27 20:47:14 -0300

    [spring-expert] criando endpoint / configurando conexao / executando sql no h2

---
+++ 01-primeiros-passos-spring-boot/produtosapi/src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
@@ -0,0 +1,9 @@
+package io.github.cursodsousa.produtosapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+}
