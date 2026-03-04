7edf923b13562f3bce54777304ce2f8bc18ad565
ThiagoRobade <robade@live.com>
2026-03-04 17:10:53 -0300

    [spring-expert] criando uma configuration e registrando um bean

--- 02-arquitetura-do-framework/arquitetura-spring/src/main/java/io/github/cursodesousa/arquiteturaspring/montadora/Carro.java
+++ 02-arquitetura-do-framework/arquitetura-spring/src/main/java/io/github/cursodesousa/arquiteturaspring/montadora/Carro.java
@@ -43,4 +43,11 @@ public class Carro {
     public void setMontadora(Montadora montadora) {
         this.montadora = montadora;
     }
+
+    public CarroStatus darIgnicao(Chave chave) {
+        if (chave.getMontadora() != this.montadora) {
+            return new CarroStatus("Não é possível ligar o carro.");
+        }
+        return new CarroStatus("Carro ligado.");
+    }
 }
