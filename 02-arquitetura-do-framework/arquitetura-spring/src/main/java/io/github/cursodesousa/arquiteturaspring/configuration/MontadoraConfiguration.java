f34fb2a212a0722885ed1627482b3868457ab068
ThiagoRobade <robade@live.com>
2026-03-19 20:32:22 -0300

    [spring-expert] utilizando qualifiers / beans primary e annotations de qualifiers

--- 02-arquitetura-do-framework/arquitetura-spring/src/main/java/io/github/cursodesousa/arquiteturaspring/configuration/MontadoraConfiguration.java
+++ 02-arquitetura-do-framework/arquitetura-spring/src/main/java/io/github/cursodesousa/arquiteturaspring/configuration/MontadoraConfiguration.java
@@ -4,18 +4,31 @@ import io.github.cursodesousa.arquiteturaspring.montadora.Motor;
 import io.github.cursodesousa.arquiteturaspring.montadora.TipoMotor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class MontadoraConfiguration {
 
-    @Bean
-    public Motor motor() {
+    @Bean(name = "motorAspirado")
+    @Primary
+    public Motor motorAspirado() {
         var motor = new Motor();
         motor.setCavalos(120);
         motor.setCilindros(4);
         motor.setModelo("XPTO-0");
         motor.setLitragem(2.0);
         motor.setTipo(TipoMotor.ASPIRADO);
+        return motor;
+    }
+
+    @Bean(name = "motorEletrico")
+    public Motor motorEletrico() {
+        var motor = new Motor();
+        motor.setCavalos(180);
+        motor.setCilindros(4);
+        motor.setModelo("TH-40");
+        motor.setLitragem(1.5);
+        motor.setTipo(TipoMotor.ELETRICO);
         return motor;
     }
 }
